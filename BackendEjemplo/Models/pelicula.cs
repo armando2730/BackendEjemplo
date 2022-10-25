0fb46a0aa20f08f213cbe435861caeb6751f25ea
Armando <oavg30gmail.com>
2022-10-24 20:26:35 -0500

    ejemplo

--- BackendEjemplo/Models/pelicula.cs
+++ BackendEjemplo/Models/pelicula.cs
@@ -7,5 +7,11 @@ namespace BackendEjemplo.Models
 {
     public class pelicula
     {
+   
+    public string Titulo { get; set; }
+    public int Duracion { get; set; }
+    public DateTime Publicacion { get; set; }
+    public string Pais { get; set; }
     }
+
 }
--- BackendEjemplo/Services/PeliculaService.cs
+++ BackendEjemplo/Services/PeliculaService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+using BackendEjemplo.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,6 +8,27 @@ using System.Web;
 namespace BackendEjemplo.Services
 {
     public class PeliculaService
+
     {
+        public List<pelicula> ObtenerPeliculas()
+        {
+
+
+            var pelicula1 = new pelicula()
+            {
+                Titulo = "Superman",
+                Duracion = 123,
+                Pais = "USA",
+                Publicacion = new DateTime(2015, 11, 12)
+            };
+            var pelicula2 = new pelicula()
+            {
+                Titulo = "Superman",
+                Duracion = 123,
+                Pais = "USA",
+                Publicacion = new DateTime(2015, 11, 12)
+            };
+            return new List<pelicula> { pelicula1, pelicula2 };
+    }
     }
 }
